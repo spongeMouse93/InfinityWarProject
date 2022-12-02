5e8dce67334d23102f7465bb999154e09c86ab18
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2022-12-02 08:32:15 -0500

    Create ForgeStormBreaker.java

---
+++ src/avengers/ForgeStormBreaker.java
@@ -0,0 +1,14 @@
+package avengers;
+
+import java.util.*;
+import java.io.*;
+
+public class ForgeStormBreaker{
+  public static void main(String[] args){
+    if (args.length < 2){
+      System.out.println("Execute: java ForgeStormBreaker <INput file> <OUTput file>");
+      return;
+    }
+    
+  }
+}
