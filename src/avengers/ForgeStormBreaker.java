8c9aee9ce0f20059f498ac06a9ea445ebc6aa116
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2022-12-02 08:36:13 -0500

    Update ForgeStormBreaker.java

--- src/avengers/ForgeStormBreaker.java
+++ src/avengers/ForgeStormBreaker.java
@@ -9,6 +9,29 @@ public class ForgeStormBreaker{
       System.out.println("Execute: java ForgeStormBreaker <INput file> <OUTput file>");
       return;
     }
-    
+    Scanner sc = new Scanner(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+    try{
+      sc = new Scanner(new File(args[0]), "UTF-8");
+    }catch (IOException e){
+      e.printStackTrace();
+    }
+    try{
+      out = new PrintWriter(new FileOutputStream(new File(args[1])), true);
+    }catch (IOException e){
+      try{
+        File f = new File(args[1]);
+        f.createNewFile();
+        out = new PrintWriter(new FileOutputStream(f), true);
+      }catch (IOException e2){
+        e2.printStackTrace();
+      }
+    }
+    int flux = 0, r = sc.nextInt(), c = sc.nextInt();
+    for (int i = 0; i < r; i++)
+      for (int j = 0; j < c; j++)
+        flux += sc.nextInt();
+    out.println(flux);
+    out.flush();
   }
 }
