2529d3d475b95cb6671e2317053bdb8f0667b5ff
Siddharth Sircar <111794453+spongeMouse93@users.noreply.github.com>
2023-03-13 20:50:34 -0400

    Update LocateTitan.java

--- src/avengers/LocateTitan.java
+++ src/avengers/LocateTitan.java
@@ -29,24 +29,19 @@ public class LocateTitan{
       }
     }
     int numGenerators = sc.nextInt();
-    int[] generators = new int[numGenerators];
+    int[] generators = new int[numGenerators], dist = new int[numGenerators];
     double[] functionalities = new double[numGenerators];
+    boolean[] djikstra = new boolean[numGenerators];
     for (int i = 0; i < numGenerators; i++){
       generators[i] = sc.nextInt();
       functionalities[i] = sc.nextDouble();
     }
     int[][] adjancency = new int[numGenerators][numGenerators];
     for (int i = 0; i < numGenerators; i++)
-      for (int j = 0; j < numGenerators; j++)
-        adjacency[i][j] = sc.nextInt();
-    for (int i = 0; i < numGenerators; i++)
       for (int j = 0; j < numGenerators; j++){
-        int energyCost = adjancency[i][j];
-        double funs = functionalities[i] * functionalities[j], x = energyCost / funs;
+        double x = adjancency[i][j] / (functionalities[i] * functionalities[j]);
         adjacency[i][j] = (int) x;
       }
-    int[] dist = new int[numGenerators];
-    boolean[] djikstra = new boolean[numGenerators];
     for (int i = 0; i < numGenerators; i++){
       dist[i] = Integer.MAX_VALUE;
       djikstra[i] = false;
@@ -59,7 +54,7 @@ public class LocateTitan{
         if (!djikstra[j] && adjancency[u][j] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + adjancency[u][j] < dist[j])
           dist[j] = dist[u] + adjancency[u][j];
     }
-    out.println(dist[dist.length - 1]);
+    out.printf("%d\n", dist[dist.length - 1]);
     out.flush();
   }
   private static int minCost(int[] dist, boolean[] djikstra){
